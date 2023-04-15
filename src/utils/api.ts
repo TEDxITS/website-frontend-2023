286392c5a1c72cba375098503afda5689c3e811f
ramadhana rizqy a <ddhanarr@gmail.com>
2023-04-15 21:30:07 +0700

    feat: auth using backend api (#35)
    * feat: add reset, forget & verify
    
    * fix: anthropocene page revision
    
    * fix: reset, forget & verify
    
    * fix: reset, forget & verify
    
    * fix: style reset, forget & verify
    
    * feat: auth revision
    
    ---------
    
    Co-authored-by: NaufalRafianto <95755846+NaufalRafianto@users.noreply.github.com>

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
-import { getToken } from '@/utils/token';
+import {
+  getAdminRefreshToken,
+  getAdminToken,
+  getRefreshToken,
+  getToken,
+} from '@/utils/token';
 
 const isServer = () => {
   return typeof window === 'undefined';
 };
 
-export const baseURL = 'https://tesdeveloper.me/api';
+export const baseURL = 'https://tedxits2023-server.azurewebsites.net/api';
 
+// Regular API
 export const api = axios.create({
   baseURL: baseURL,
   headers: {
@@ -24,5 +30,94 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+
+api.interceptors.response.use(
+  async (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    const response = error.response;
+
+    if (
+      response &&
+      response.status === 401 &&
+      response.data.message === 'Invalid Token' &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = getRefreshToken();
+        const newAccessTokenResponse = await axios.get('/refresh-token', {
+          headers: {
+            Authorization: refreshToken ? `Bearer ${refreshToken}` : '',
+          },
+        });
+
+        const newAccessToken = newAccessTokenResponse.data.accessToken;
+
+        // Update the original request with the new access token
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        // Resend the original request
+        return api(originalRequest);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+// Admin API
+export const adminApi = axios.create({
+  baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: false,
+});
+
+adminApi.interceptors.request.use((config) => {
+  if (!isServer() && config.headers) {
+    const token = getAdminToken();
+    config.headers.Authorization = token ? `Bearer ${token}` : '';
+  }
+  return config;
+});
+
+adminApi.interceptors.response.use(
+  async (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    const response = error.response;
+
+    if (
+      response &&
+      response.status === 401 &&
+      response.data.message === 'Invalid Token' &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = getAdminRefreshToken();
+        const newAccessTokenResponse = await axios.get('/admin-refresh-token', {
+          headers: {
+            Authorization: refreshToken ? `Bearer ${refreshToken}` : '',
+          },
+        });
+
+        const newAccessToken = newAccessTokenResponse.data.accessToken;
+
+        // Update the original request with the new access token
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        // Resend the original request
+        return api(originalRequest);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
--- src/utils/token.ts
+++ src/utils/token.ts
@@ -23,3 +23,31 @@ export const removeToken = () => {
 export const setToken = (token: string) => {
   Cookies.set('tedxits2023-token', token);
 };
+
+export const getAdminToken = (): string => {
+  return Cookies.get('tedxits2023-admin-token') || '';
+};
+
+export const removeAdminToken = () => {
+  return Cookies.remove('tedxits2023-admin-token');
+};
+
+export const setAdminToken = (token: string) => {
+  Cookies.set('tedxits2023-admin-token', token);
+};
+
+export const setRefreshToken = (token: string) => {
+  Cookies.set('tedxits2023-refresh-token', token);
+};
+
+export const setAdminRefreshToken = (token: string) => {
+  Cookies.set('tedxits2023-admin-refresh-token', token);
+};
+
+export const getRefreshToken = (): string => {
+  return Cookies.get('tedxits2023-refresh-token') || '';
+};
+
+export const getAdminRefreshToken = (): string => {
+  return Cookies.get('tedxits2023-admin-refresh-token') || '';
+};
