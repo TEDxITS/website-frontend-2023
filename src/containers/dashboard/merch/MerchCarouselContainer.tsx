cb182fc24f1d6dfd99dd0d5b583f68ba95fc80b4
Daud Dhiya <daud.dhiya31@gmail.com>
2023-05-12 16:31:28 +0700

    feat: add carousel merch

--- src/containers/dashboard/merch/MerchCarouselContainer.tsx
+++ src/containers/dashboard/merch/MerchCarouselContainer.tsx
@@ -1,23 +1,118 @@
+import Image from 'next/image';
+import { useState } from 'react';
+import { GrNext, GrPrevious } from 'react-icons/gr';
+
+import { currencyFormat } from '@/utils/currency';
+
 import { MerchTypes } from '@/types/merch.types';
+
+import LeftSide from '~/images/merch/left-side.png';
+import RightSide from '~/images/merch/right-side.png';
 
 interface MerchProps {
   merchandise: MerchTypes[];
 }
 
 export default function MerchCarouselContainer(props: MerchProps) {
+  const [lastShow, setLastShow] = useState(1);
+  // console.log(props.merchandise);
+
   return (
-    <div>
-      {props.merchandise.map((merch: MerchTypes) => (
-        <div key={merch.id}>
-          {/* <img src={merch.image} /> */}
-          <p>{merch.name}</p>
-          <p>{merch.type}</p>
-          <p>
-            <span>Rp.</span>
-            {merch.price}
-          </p>
+    <div className='flex h-screen w-full items-center justify-center overflow-hidden'>
+      <Image
+        src={LeftSide}
+        alt='LeftSide'
+        className='absolute -left-72 scale-90'
+      />
+      <Image
+        src={RightSide}
+        alt='RightSide'
+        className='absolute -right-72 scale-90'
+      />
+
+      <GrPrevious
+        onClick={() => {
+          if (lastShow === 1) {
+            setLastShow(2);
+          } else {
+            setLastShow(1);
+          }
+        }}
+        className='z-30 cursor-pointer rounded-full bg-cwhite/50 p-1 text-xl duration-200 hover:bg-cwhite md:p-3 md:text-[50px]'
+      />
+
+      <div className='flex h-screen w-full items-center justify-center overflow-hidden'>
+        <div
+          className={`
+          ${lastShow === 1 ? `z-10 scale-150` : 'z-0 opacity-70'} 
+          flex translate-x-28 flex-col items-center justify-center gap-4 p-10 duration-300 md:translate-x-32
+        `}
+        >
+          <h2
+            className={`${
+              lastShow == 1 ? 'flex' : 'hidden'
+            } font-quaker text-base md:text-2xl`}
+          >
+            {props.merchandise[4].name}
+          </h2>
+          <div className='flex h-24 w-36 items-center justify-center bg-gradient-to-r from-cwhite via-cblue to-cblack md:h-48 md:w-72'>
+            <Image
+              src={props.merchandise[4].image}
+              alt='...'
+              width={200}
+              height={200}
+            />
+          </div>
+          <span
+            className={`${
+              lastShow == 1 ? 'flex' : 'hidden'
+            } rounded-3xl bg-gradient-to-r from-cwhite via-cblue to-cblack px-4 py-1 font-primary text-xs font-bold md:text-lg`}
+          >
+            {currencyFormat(props.merchandise[4].price)}
+          </span>
         </div>
-      ))}
+
+        <div
+          className={`
+          ${lastShow === 2 ? `z-10 scale-150` : 'z-0 opacity-70'} 
+          flex -translate-x-28 flex-col items-center justify-center gap-4 p-10 duration-300 md:-translate-x-32
+      `}
+        >
+          <h2
+            className={`${
+              lastShow == 2 ? 'flex' : 'hidden'
+            } font-quaker text-base md:text-2xl`}
+          >
+            {props.merchandise[5].name}
+          </h2>
+          <div className='flex h-24 w-36 items-center justify-center bg-gradient-to-r from-cwhite via-cblue to-cblack md:h-48 md:w-72'>
+            <Image
+              src={props.merchandise[4].image}
+              alt='...'
+              width={200}
+              height={200}
+            />
+          </div>
+          <span
+            className={`${
+              lastShow == 2 ? 'flex' : 'hidden'
+            } rounded-3xl bg-gradient-to-r from-cwhite via-cblue to-cblack py-1 px-4 font-primary text-xs font-bold md:text-lg`}
+          >
+            {currencyFormat(props.merchandise[5].price)}
+          </span>
+        </div>
+      </div>
+
+      <GrNext
+        onClick={() => {
+          if (lastShow === 1) {
+            setLastShow(2);
+          } else {
+            setLastShow(1);
+          }
+        }}
+        className=' z-30 cursor-pointer rounded-full bg-cwhite/50 p-1 text-xl duration-200 hover:bg-cwhite md:p-3 md:text-[50px]'
+      />
     </div>
   );
 }
--- src/containers/dashboard/merch/MerchGridContainer.tsx
+++ src/containers/dashboard/merch/MerchGridContainer.tsx
@@ -2,7 +2,7 @@
 
 import { useQuery } from '@tanstack/react-query';
 
-// import MerchCarouselContainer from '@/containers/dashboard/merch/MerchCarouselContainer';
+import MerchCarouselContainer from '@/containers/dashboard/merch/MerchCarouselContainer';
 import MerchItemContainer from '@/containers/dashboard/merch/MerchItemContainer';
 
 import { localApi } from '@/utils/local-api';
@@ -31,7 +31,7 @@ export default function MerchGridContainer() {
   return (
     <div className='flex min-h-screen w-full flex-col p-4 text-white'>
       <span className='mx-auto text-2xl'>Merch Catalogue</span>
-      {/* <MerchCarouselContainer merchandise={merchQuery.data.data} /> */}
+      <MerchCarouselContainer merchandise={merchQuery.data.data} />
       <MerchItemContainer merchandise={merchQuery.data.data} />
     </div>
   );
